e13a7b9f17b14a29d2c65185e9c10281ac735715
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-01-02 17:23:31 +0900

    :tada: first

---
+++ src/main/java/com/minjae/minjijik/MinjijikApplication.java
@@ -0,0 +1,13 @@
+package com.minjae.minjijik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinjijikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinjijikApplication.class, args);
+	}
+
+}
